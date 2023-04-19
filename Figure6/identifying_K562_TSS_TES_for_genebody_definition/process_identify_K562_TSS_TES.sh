7a8fd266646d67484686fde08817a2d469afc3c6
Jin Woo Oh <64922399+oh-jinwoo94@users.noreply.github.com>
2023-04-19 16:44:17 -0400

    Update process_identify_K562_TSS_TES.sh

--- Figure6/identifying_K562_TSS_TES_for_genebody_definition/process_identify_K562_TSS_TES.sh
+++ Figure6/identifying_K562_TSS_TES_for_genebody_definition/process_identify_K562_TSS_TES.sh
@@ -30,3 +30,7 @@ python combine_TSS_TES_best.py isoform_index_with_K562_pol2_TSS.out isoform_inde
 
 
 # for K562 HCRFF covered genes, this method unambiguously provided us the highest confidence gene bodies. 
+
+
+# to generate list of K562 genes w/ their coordinates. Genes with multiple gene coordinates with equally high TSS/TES Pol2 enrichment are duplicated. 
+python3 index_to_TSSfile.py  K562_best_isoform_indices.txt  /mnt/t/data0/joh27/projects/ENCODE/ref_genome/gene_annotation/refGene.txt|uniq > K562_gene_bodies_selected_by_higest_Pol2_TSS_TES.out
